5eb01022c5732812d1547881560af1d5f225ed30
Mariam <mariam.araqelyan2004@gmail.com>
2023-06-06 16:15:30 +0400

    something I needed to calculate

---
+++ exercise_3.py
@@ -0,0 +1,23 @@
+print("We will now count some fruits:")
+
+print("Apples", 30 + 70 % 3)
+print("Bananas", 45 * 11 -14 / 5)
+
+print("Now we'll count the mandarines: ")
+
+print(14 - 9 + 5 % 9 + 4 / 2 + 1 * 8 + 18)
+
+print("Is it true that 7 + 9 < 3 - 1?")
+
+print(7 + 9 < 3 - 1)
+
+print("What is 7 / 2?", 7 / 2)
+print("What is 16 - 5?", 16 - 5)
+
+
+
+
+
+print("Is 3.5 greater than 11?", 3.5 > 11)
+print("Is it greater or equal?", 3.5 >= 11)
+print("Is it less or equal?", 3.5 <= 11)

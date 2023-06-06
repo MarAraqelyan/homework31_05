8a26e5dfc97f02e80bedc32f22bc7ed6c2fab100
Mariam <mariam.araqelyan2004@gmail.com>
2023-06-06 16:21:11 +0400

    changing variable names and adding variables

---
+++ ex5.py
@@ -0,0 +1,18 @@
+name = 'Zed A. Shaw'
+age = 35
+height = 74
+weight = 180
+eye_color = 'Blue'
+teeth_color = 'White'
+hair_color = 'Brown'
+inches_to_cm = 74 * 2.54
+pounds_to_kg = 180 * 0.453592
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches or {inches_to_cm} tall .")
+print(f"He's {weight} pounds or {pounds_to_kg}.")
+print("Actually that's not too heavy.")
+print(f"He's got {eye_color} eyes and {hair_color} hair.")
+print(f"His teeth are usually {teeth_color} depending  on the coffee.")
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")

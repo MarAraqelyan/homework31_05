5477403307a7a75bb60516739e1fb0e4a8462e1f
Mariam <mariam.araqelyan2004@gmail.com>
2023-06-14 15:54:01 +0400

    read and copy fixed

--- read_copy.py
+++ read_copy.py
@@ -1,4 +1,17 @@
 f = open('reaganomics.txt')
-for i in range(70):
+
+sentence ="Federal income tax and payroll tax levels" 
+lines = f.readlines()
+for line in lines:
+	if line.find(sentence) != -1:
+		x = lines.index(line)
+
+f.seek(0)
+for i in range(x):
 	text = f.readline()
 	print(text)
+
+f.close()
+		
+
+
